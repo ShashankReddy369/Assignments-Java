ebc7d85d8a1fcdba51d2fdabc0354e4def8907d6
ShashankReddy369 <90498773+ShashankReddy369@users.noreply.github.com>
2021-09-19 09:49:51 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class sum {
+
+	public static void main(String[] args) {
+		
+		int x= Integer.parseInt(args[0]);//8
+		int y= Integer.parseInt(args[1]);//9
+		int s= x+y;
+		System.out.println("Sum is:"+s);//17
+
+	}
+
+}
